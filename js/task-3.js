eefebe177bdfc02953d1cc1c15917186cfc26a3b
Alex <o.pruzhanskiy@gmail.com>
2024-12-12 15:00:24 +0200

    fix

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value) {
+    const result = [];
+    for (const elem of numbers) {
+        if (elem > value) {
+            result.push(elem);
+        }
+    }
+    return result;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
